19c2b7f59697adccbcf8bbeca8a2970a8942b4b5
Adam Mertz <adammertz@gmail.com>
2017-12-03 17:51:21 -0600

    Removed header file from extension

--- ext/euler_py.c
+++ ext/euler_py.c
@@ -2,6 +2,8 @@
 #include <Python.h>
 #include "../src/euler.h"
 
+static char module_docstring[] = "Provides interface to Project Euler problems";
+
 static PyObject* euler_py_problem_one(PyObject *self, PyObject *args)
 {
     int max, result;
@@ -13,3 +15,25 @@ static PyObject* euler_py_problem_one(PyObject *self, PyObject *args)
 
     return Py_BuildValue("i", result);
 }
+
+static PyMethodDef module_methods[] = {
+        {"problem_one", euler_py_problem_one, METH_VARARGS, "Solution to problem 1"},
+        {NULL, NULL, 0, NULL}
+};
+
+
+// Module definition
+PyObject* euler_pymodule = {
+        PyModuleDef_HEAD_INIT,
+        "euler_py",
+        module_docstring,
+        -1,
+        module_methods
+};
+
+// Module initialization function
+PyMODINIT_FUNC PyInit_euler_py(void)
+{
+    return PyModule_Create(&euler_pymodule);
+}
+
