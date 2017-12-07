0dd217c9e0a061e3622fd150b61e24a2c6bad5af
Adam Mertz <adammertz@gmail.com>
2017-12-06 18:16:31 -0600

    Wrote test cases for other 3 problems

--- py_test.py
+++ py_test.py
@@ -4,3 +4,18 @@ import euler_py as eul
 def test_problem_one():
     result = eul.problem_one(10)
     assert result == 23, f'Problem 1 should be 23: {result}'
+
+
+def test_problem_two():
+    result = eul.problem_two(89)
+    assert result == 44, f'Problem 2 should be 44: {result}'
+
+
+def test_problem_three():
+    result = eul.problem_three(13195)
+    assert result == 29, f'Problem 3 should be 29: {result}'
+
+
+def test_problem_four():
+    result = eul.problem_four(2)
+    assert result == 9009, f'Problem 4 should be 9009: {result}'
